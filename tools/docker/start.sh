2b4ccae20e6e6cea26cbe61d4b8a7fb66384fd1e
liuzheng <liuzheng712@gmail.com>
2020-12-31 23:18:45 +0800

    feat: update

--- tools/docker/start.sh
+++ tools/docker/start.sh
@@ -9,22 +9,23 @@ if [ ! -f ${CONFIG_FILE} ]; then
       --output-file "${CONFIG_FILE}"
     INI_SET="crudini --set ${CONFIG_FILE}"
 
-    ${INI_SET} DEFAULT js_implementation py_mini_racer
-    ${INI_SET} DEFAULT auth_type ${DEFAULT.auth_type}
-    ${INI_SET} DEFAULT transport_url "${DEFAULT.transport_url}"
-    ${INI_SET} DEFAULT debug "${DEFAULT.debug}"
+    ${INI_SET} DEFAULT js_implementation "py_mini_racer"
+    ${INI_SET} DEFAULT auth_type  "$(perl -e 'print $ENV{"DEFAULT.auth_type"}')"
+    ${INI_SET} DEFAULT transport_url "$(perl -e 'print $ENV{"DEFAULT.transport_url"}')"
+    ${INI_SET} DEFAULT debug $(perl -e 'print $ENV{"DEFAULT.debug"}')
 
-    ${INI_SET} pecan auth_enable ${pecan.auth_enable}
+    ${INI_SET} pecan auth_enable $(perl -e 'print $ENV{"pecan.auth_enable"}')
 
-    ${INI_SET} keycloak_oidc auth_url ${keycloak_oidc.auth_url}
-    ${INI_SET} keycloak_oidc insecure ${keycloak_oidc.insecure}
+    ${INI_SET} keycloak_oidc auth_url "$(perl -e 'print $ENV{"keycloak_oidc.auth_url"}')"
+    ${INI_SET} keycloak_oidc insecure $(perl -e 'print $ENV{"keycloak_oidc.insecure"}')
 
-    ${INI_SET} database connection "${database.connection}"
-    ${INI_SET} oslo_policy policy_file "${oslo_policy.policy_file}"
+    ${INI_SET} database connection "$(perl -e 'print $ENV{"database.connection"}')"
+    ${INI_SET} oslo_policy policy_file "$(perl -e 'print $ENV{"oslo_policy.policy_file"}')"
 fi
 
-if [ ${database.connection} == "sqlite:///data/mistral.db" -a ! -f /data/mistral.db ]
+if [ "$(perl -e 'print $ENV{"database.connection"}')" == "sqlite:///data/mistral.db" -a ! -f /data/mistral.db ]
 then
+    mkdir -p /data/
     python ./tools/sync_db.py --config-file "${CONFIG_FILE}"
     mistral-db-manage --config-file "${CONFIG_FILE}" populate
 fi
